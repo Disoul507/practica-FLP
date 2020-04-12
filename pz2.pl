9a2b7eb264edeb5da29301250782754cec09278e
Disoul507 <62339531+Disoul507@users.noreply.github.com>
2020-04-12 12:55:39 +0500

    Create pz2.pl

---
+++ pz2.pl
@@ -0,0 +1,25 @@
+%нахождение max
+max(X,Y,X):-X>Y,!.
+max(_,Y,Y).
+%нахождение max при 3-х переменных
+max3(X,Y,U,Z):-max(X,U,K),max(Y,K,Z).
+%Факториал
+fact(0,1):-!.
+fact(N,X):- N1 is N-1,fact(N1,X1),X is X1*N.
+%Второй вариант
+fact1(N,X):-fact2(0,1,N,X).
+fact2(N,K,N,K):-!.
+fact2(I,K,N,X):-I1 is I+1, K1 is K*I1, fact2(I1,K1,N,X).
+%фибоначчи
+fib(1,1):-!.
+fib(2,1):-!.
+fib(N,X):- N1 is N-1,fib(N1,X1),N2 is N-2,fib(N2,X2),X is X1+X2.
+%Второй вариант
+fib1(N,X):-fib2(1,1,2,N,X).
+fib2(_K,N,N,K):-!.
+fib2(J,K,I,N,X):- I1 is I+1,K1 is K+J,fib2(K,K1,I1,N,X).
+%Проверка на пустоту
+pr(2):-!.
+pr(X):-pr1(2,X).
+pr1(X,X):-!.
+pr1(I,X):-Y is X mod I,not(y=0),I1 is I+1,pr1(I1,X).
