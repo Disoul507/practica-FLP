0ba4608f83780f1b3ef2c1bba520b29711dcf289
Disoul507 <62339531+Disoul507@users.noreply.github.com>
2020-06-07 03:01:22 +0500

    Update Program.fs

--- IZ9/IZ9/Program.fs
+++ IZ9/IZ9/Program.fs
@@ -4,14 +4,18 @@ let rec izbyt s x i =
     else if x%i = 0 then (izbyt (s+i) x (i+1))
     else (izbyt s x (i+1))
 
-let rec kol_izb f x tek =
-    if x=20000 then tek
-    else if (f 0 x 1)>x then (kol_izb f (x+1) (tek+1))
-    else (kol_izb f (x+1) tek)
+let kolIzb f =
+    let rec kol_izb f x tek =
+        if x=20000 then tek
+        else if (f 0 x 1)>x then 
+       //System.Console.Write(System.Convert.ToString(x)+"\n");
+            (kol_izb f (x+1) (tek+1))
+        else (kol_izb f (x+1) tek)
+    kol_izb f 1 0
 
 [<EntryPoint>]
 let main argv =
-    let result = kol_izb izbyt 1 0
+    let result = kolIzb izbyt
     System.Console.Write("Количество избыточных чисел: " + System.Convert.ToString(result));
     let z = System.Console.ReadKey() 
-    0 // return an integer exit code
+    0 
